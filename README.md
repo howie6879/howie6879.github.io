b508e4fe9e8caeb2d692481666b336d71ac1d1ee
howie6879 <howie6879@gmail.com>
2023-02-03 18:39:42 +0800

    clean git

---
+++ README.md
@@ -0,0 +1,14 @@
+## Howie's blog
+
+奇文共欣赏，疑义相与析：
+
+- 我的博客：[howie6879](https://www.howie6879.cn/)
+- 我的周刊：[weekly](https://weekly.howie6879.cn/)
+- 我的小书：
+  - [k8s 学习之路](https://www.howie6879.cn/k8s/)
+  - [ML Note](https://www.howie6879.cn/ml_book/)
+  - [Sanic 小书](https://www.howie6879.cn/sanic_book/)
+
+最后，欢迎一起交流：
+
+<div align=center><img width="300px" height="300px" src="https://images-1252557999.file.myqcloud.com/uPic/ETIbMe.jpg" /></div>
---
+++ ruia/en.search.min.e1b6ddaa73c1909f28b36f507884ccccb7597eef98477171b190b95d3cb96f5c.js
@@ -0,0 +1,11 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();event.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/ruia/flexsearch.min.js');loadScript('/ruia/en.search-data.min.8c4c3739045533786def7834354d61fb1b6c6fa13d92260f5fdf15ff5b5820f4.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}
+function element(content){const div=document.createElement('div');div.innerHTML=content;return div.firstChild;}})();
